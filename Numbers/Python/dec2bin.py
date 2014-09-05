f038df2a25057cb75c6ea0b9ef28c1e1f082cd2b
Alexandre Souza <aesouza.jvm@gmail.com>
2014-09-05 09:30:08 -0300

    Update dec2bin.py

--- Numbers/Python/dec2bin.py
+++ Numbers/Python/dec2bin.py
@@ -1,3 +1,7 @@
+'''
+Binary to Decimal and Back Converter -
+Develop a converter to convert a decimal number to binary or a binary number to its decimal equivalent.
+'''
 conv = []
 def dec2bin(num):
     '''
