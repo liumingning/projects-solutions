d7ed5f94da3bb082c336d540724890c933020f08
Alexandre Souza <aesouza.jvm@gmail.com>
2014-09-05 09:29:14 -0300

    Update fibseq.py

--- Numbers/Python/fibseq.py
+++ Numbers/Python/fibseq.py
@@ -1,3 +1,7 @@
+'''
+Fibonacci Sequence -
+Enter a number and have the program generate the Fibonacci sequence to that number or to the Nth number.
+'''
 seq = [0,1]
 def fibseq(maxnum, item=1):
     (item <= maxnum and
