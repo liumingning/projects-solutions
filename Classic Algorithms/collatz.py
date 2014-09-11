6313df949ad04586a7c2e5138232a8a7b560b65f
Lucas <lucasdnd@gmail.com>
2014-09-11 19:03:01 -0300

    Code improvements

--- Classic Algorithms/collatz.py
+++ Classic Algorithms/collatz.py
@@ -2,6 +2,9 @@
 # Start with a number n > 1. Find the number of steps it takes to reach one using
 # the following process: If n is even, divide it by 2. If n is odd, multiply it by 3 and add 1.
 
+import sys
+input = input if sys.version_info.major == 3 else raw_input
+
 def collatz(num, steps):
 
   if num % 2 == 0:
@@ -13,11 +16,8 @@ def collatz(num, steps):
   steps += 1
   print "[" + str(steps) + "] = " + str(num)
 
-  if num != 1:
-    return collatz(num, steps)
+  return collatz(num, steps) if num != 1 else 1
 
-  return 1
-
-num = int(raw_input("Enter a number: "))
+num = int(input("Enter a number: "))
 steps = 0
 collatz(num, steps)
